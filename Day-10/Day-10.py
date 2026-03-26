345eda6214824dfd355643be4b419cff5335ed71
Your Name <your.email@example.com>
2026-03-26 17:29:11 +0530

    Update Day-10.py

--- Day-10/Day-10.py
+++ Day-10/Day-10.py
@@ -8,4 +8,15 @@ i=[i for i in range(1,100) if i%2==0]
 '''
 #List Comprehension
 #Lamda Function
-print(i)
+
+#Generator Expression
+def reels():
+    r = ['1..100','101..200','201..300','301..400','401..500']
+    for i in r:
+        yield i
+
+scroll = reels()
+
+print(next(scroll))
+print(next(scroll))
+print(next(scroll))
