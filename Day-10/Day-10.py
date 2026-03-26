4a339f8b7e027c1798512a350b4ec2c676fc550d
Your Name <your.email@example.com>
2026-03-26 18:01:48 +0530

    Update Day-10.py

--- Day-10/Day-10.py
+++ Day-10/Day-10.py
@@ -10,7 +10,7 @@ i=[i for i in range(1,100) if i%2==0]
 #Lamda Function
 
 #Generator Expression
-def reels():
+'''def reels():
     r = ['1..100','101..200','201..300','301..400','401..500']
     for i in r:
         yield i
@@ -20,3 +20,19 @@ scroll = reels()
 print(next(scroll))
 print(next(scroll))
 print(next(scroll))
+'''
+
+def dispaly():
+    yield "pfs-50"
+    yield "pfs-51"
+    yield "pfs-52"
+    yield "pfs-53"
+    yield "pfs-54"
+    yield "pfs-55"
+    yield "pfs-56"
+    yield "pfs-57"
+
+leave = dispaly()
+
+for i in range(8):
+    print(next(leave))
